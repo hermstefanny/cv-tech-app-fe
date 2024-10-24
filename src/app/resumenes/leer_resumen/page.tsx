a3d657c1931e4fd5d1f6052e181ed5045380d8ce
hermstefanny <aida.estefania.sosa@gmail.com>
2024-10-24 10:57:34 +0200

    Refactor font styles and layout in leer_resumen component

--- src/app/resumenes/leer_resumen/page.tsx
+++ src/app/resumenes/leer_resumen/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 import Link from 'next/link';
+import Image from 'next/image';
+import docLinkImg from 'public/link-document.png';
 import { Suspense, useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 
@@ -23,15 +25,35 @@ function SummaryPage() {
         const resp = await response.json();
         const summary: Summary = resp.summary;
         const data = <div>
-          <h3><strong>{summary.act_name}</strong></h3>
-          <p><strong>Fecha:</strong> {new Date(summary.date).toLocaleDateString()}</p>
-          <p><strong>Resumen:</strong></p>
-          <ul>
-            {summary.content && summary.content.map((content, index) => (
-              <li key={index}>{content}</li>
+          <div className="w-full lg:max-w-6xl rounded-lg shadow-md p-2 overflow-hidden border leading-relaxed mt-4">
+          <h3 className =" text text-sky-600 text-xl text-center lg:text-3xl p-2" ><strong>{summary.act_name}</strong></h3>
+          <div className="flex flex-col lg:flex-row w-full lg:max-w-6xl items-center  overflow-hidden">
+          <div className="lg:w-2/3 w-full flex  p-4 lg:p-4">
+          <div>
+          <p className = "text-sky-800 font-semibold p-2" ><strong>Fecha:</strong> {new Date(summary.date).toLocaleDateString('es-ES', {day:'numeric', month:'long', year: 'numeric'})}</p>
+           <ul>
+            {summary.content && summary.content.map((content, index) => ( 
+              <div className= "flex flex-wrap p-2 ">
+              <li className ="text-sky-800 text-sm md:text-sm lg:text-base  bg-sky-100/80 p-2  lg:p-4 leading-relaxed shadow-md" key={index}>{content}</li>
+              </div>
             ))}
           </ul>
+          </div>
+          </div>
+          <div className="lg:w-1/3 w-full flex flex-col items-center p-2 lg:p-4 space-y-4">
+          <Image
+            src={docLinkImg}
+            alt="Ilustracion con el documento saliendo de un laptop"
+            className="object-contain max-h-96"
+          />
+          <button className="border rounded p-2 bg-amber-600 text-white shadow-md mb-2  hover:bg-amber-800 transition ">
+            <Link href={summary.document_url} target="_blank" rel="noopener noreferrer">LINK AL DOCUMENTO ORIGINAL</Link>
+          </button>          
+          </div>
 
+          </div>
+          </div>
+        
         </div>
         setProcessedSummary(data)
       } catch (error) {
@@ -56,7 +78,7 @@ export default function Page() {
         <SummaryPage />
       </Suspense>
       <div className="flex justify-center mt-6 p-4">
-        <button className="border rounded p-2 bg-sky-600 text-white mb-2 hover:bg-sky-700 transition">
+        <button className="border rounded p-2 bg-sky-600 text-white mb-2 shadow-md hover:bg-sky-700 transition">
           <Link href="/resumenes">Regresar</Link>
         </button>
       </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,9 @@ export default function Header(){
     return(
         <div className = "w-full absolute text-white bg-sky-800 z-10">
             <nav className = "container relative flex flex-wrap items-center justify-between mx-auto p-6 ">
-            <Link href="/" className = "font-bond text-2xl font-montserrat font-semibold hover:text-sky-400 ">Inicio</Link>
+            <Link href="/" className = "font-bond text-2xl font-open-sans font-semibold hover:text-sky-400 ">CiviConecta</Link>
             <div className = "space-x-3 text-lg">
-            <Link href="/resumenes" className = "font-montserrat font-semibold  hover:text-sky-400 ">Resúmenes</Link>
+            <Link href="/resumenes" className = "font-montserrat font-semibold  hover:text-sky-400 ">Actas del Concejo de Quito </Link>
             <Link href="/acerca_de" className = "font-montserrat font-semibold  hover:text-sky-400">Acerca del Proyecto</Link>
             </div>
             </nav>
